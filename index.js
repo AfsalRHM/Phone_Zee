1e9c778e471826ee42a71952299d5202fb68b3fb
Afsal Rahman <afsalrahman@gmail.com>
2024-06-21 11:52:32 +0530

    trial

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine","ejs");
 
+// kjbdsvkhsbvkbkvbds-
 // for user routes
 const userRoute = require('./routes/userRoutes');
 app.use('/', userRoute);
