7cd004181e00a112aa9471b4122b0a3f84969713
Afsal Rahman <afsalrahman@gmail.com>
2024-06-22 09:58:23 +0530

    Problem Solving 2

--- passport.js
+++ passport.js
@@ -18,7 +18,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new googleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "http://phonezee.live/auth/google/callback",
     passReqToCallback: true
 },
 function(request, accessToken, refreshToken, profile, done) {
